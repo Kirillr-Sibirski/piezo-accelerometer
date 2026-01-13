5e78bc0048868739a9b841cb8156c6f2f65b270f
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2026-01-13 12:02:44 +0100

    Update scalor. Add python UART reader

---
+++ info-display/read_uart.py
@@ -0,0 +1,38 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "matplotlib>=3.10.8",
+#     "serial>=0.0.97",
+# ]
+# ///
+
+import serial
+import matplotlib.pyplot as plt
+
+# Configuring the serial port
+ser = serial.Serial('/dev/cu.usbmodem1101', 115200) # Might need to change this
+
+values = []
+
+plt.ion()  # interactive mode
+fig, ax = plt.subplots()
+
+try:
+    while True:
+        line = ser.readline().decode().strip()  # read one line from UART
+        if line:
+            try:
+                value = int(line)
+                values.append(value)
+                if len(values) > 100:  # keep last 100 points
+                    values.pop(0)
+                ax.clear()
+                ax.plot(values)
+                ax.set_ylim(0, 4096)  # ADC full scale
+                plt.pause(0.01)
+            except ValueError:
+                pass  # ignore bad lines
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    ser.close()
