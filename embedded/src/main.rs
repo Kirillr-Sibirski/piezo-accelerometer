1993c3b122f49e64306c59436e5104f76d7ebf00
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2026-01-20 09:56:52 +0100

    Convert peak voltage to pp

--- embedded/src/main.rs
+++ embedded/src/main.rs
@@ -3,7 +3,7 @@ use esp_idf_hal::adc::oneshot::{config::AdcChannelConfig, AdcChannelDriver, AdcD
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_sys::{self as sys, *};
 use std::thread;
-use std::time::Duration; // Import for raw C APIs
+use std::time::Duration;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -20,8 +20,10 @@ fn main() -> anyhow::Result<()> {
     let mut adc_pin = AdcChannelDriver::new(&mut adc, peripherals.pins.gpio4, &config)?;
 
     // Linear fit graph
-    const gradient: f64 = -0.00976875;
-    const y-intercept: f64 = 4.023748;
+    const GRADIENT: f64 = -0.00976875;
+    const Y_INTERCEPT: f64 = 4.023748;
+    const SAMPLES_PER_WINDOW: usize = 50;
+    const BIAS_VOLTAGE: f64 = 2500.0; // mV - the circuit's DC offset
 
     // Set up calibration characteristics (done once outside the loop)
     let mut chars: esp_adc_cal_characteristics_t = unsafe { std::mem::zeroed() };
@@ -35,10 +37,9 @@ fn main() -> anyhow::Result<()> {
         )
     };
 
-    // Optional: Check if calibration is supported/useful
+    // Check if calibration is supported/useful
     if cal_type == sys::esp_adc_cal_value_t_ESP_ADC_CAL_VAL_NOT_SUPPORTED {
         println!("Warning: ADC calibration not supported on this chip; using raw estimates.");
-        // You could fall back to manual scaling here if needed
     }
 
     loop {
@@ -47,10 +48,13 @@ fn main() -> anyhow::Result<()> {
         // Convert raw to calibrated voltage (in mV)
         let v_measured = unsafe { esp_adc_cal_raw_to_voltage(raw as u32, &chars) };
 
-        let v_out = v_measured / SCALAR; // Scale it by the voltage divider factor
-        let acceleration = gradient*v_out+y-intercept;
+        let v_peak = (v_measured - BIAS_VOLTAGE).abs(); // Peak amplitude
+        let v_pp = v_peak * 2.0; // Convert to peak to peak = 2 * peak
 
-        println!("{:.2}", acceleration);
+        // let v_out = v_measured / SCALAR; // Scale it by the voltage divider factor
+        let acceleration = GRADIENT * (v_pp as f64) + Y_INTERCEPT;
+
+        println!("{:.2}", acceleration); //in g
 
         thread::sleep(Duration::from_millis(100));
     }
